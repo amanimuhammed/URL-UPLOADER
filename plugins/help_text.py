00b3a41c6508d88285fd76c2e035af0fe4574534
Amani Muhammed <71679579+amanimuhammed@users.noreply.github.com>
2021-03-03 09:05:09 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -27,13 +27,3 @@ async def start(bot, update):
     )
 
 
-@Client.on_message(filters.command(["about"]))
-def about(bot, update):
-    
-    bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.ABOUT_TEXT,
-        parse_mode="html",
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True   
-    ) 
