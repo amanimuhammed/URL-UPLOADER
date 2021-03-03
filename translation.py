ea20621840522fec022fbebde3d0a5ca67288f1a
Amani Muhammed <71679579+amanimuhammed@users.noreply.github.com>
2021-03-03 09:04:35 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -25,19 +25,6 @@ Owner : @Amani_m_h_d
 Owner : @Amani_m_h_d
 """
 
-     ABOUT_TEXT = """ Hai guys...
-
-➠<b>My Name :</b> <code>URL UPLOADER🤓</code>
-➠<b>Dev :</b> <a href='https://t.me/Amani_m_h_d'>Amani Muhammed</a>
-➠<b>Credits :</b> <code>Everyone in this journey</code>
-➠<b>Language :</b> <code>Python3</code>
-➠<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-➠<b>Server :</b> <a href='https://herokuapp.com/'>Heroku</a>
-➠<b>Source Code :</b> 👉 <a href='http://t.me/nokkiirunnoippokittum'>Click Here</a>
-   
-      <b>📜Quote :</b> <code>ആരും പേടിക്കണ്ട എല്ലാവർക്കും കിട്ടും™️</code>"""
-
-
     FORMAT_SELECTION = """Select the desired format: <a href='{}'>file size might be approximate</a>
     
 Send your custum thumbnail if required.
